6f20e4c0f739d5a9a8171d327c847fb2b8bea643
akashbhatc <117592488+akashbhatc@users.noreply.github.com>
2024-05-16 12:42:43 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { React } from "react";
+import React from "react";
 import Login from "./pages/auth/login/Loginpage";
 import Signuppage from "./pages/auth/signup/Signuppage";
-import { BrowserRouter as  Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-      <div>
+    <div>
+      <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signuppage />} />
         </Routes>
-      </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
