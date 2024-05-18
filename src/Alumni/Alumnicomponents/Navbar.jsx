3c353d1c928d2d525dfa86c5bc70f09afeb1688e
akashbhatc <117592488+akashbhatc@users.noreply.github.com>
2024-05-18 10:26:22 +0530

    a

--- src/Alumni/Alumnicomponents/Navbar.jsx
+++ src/Alumni/Alumnicomponents/Navbar.jsx
@@ -95,7 +95,7 @@ export const Navbar = () => {
         <ul className="space-y-2 font-medium">
           <li>
             <a
-              href="/student/dashboard"
+              href="/alumni/dashboard"
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img
@@ -107,7 +107,7 @@ export const Navbar = () => {
           </li>
           <li>
             <a
-              href="/student/queries"
+              href="/alumni/queries"
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img
