85b1ce021a783dfa43ccc151ad1755a2da8e9eef
akashbhatc <117592488+akashbhatc@users.noreply.github.com>
2024-05-25 12:45:17 +0530

    responsive signup

--- src/pages/auth/signup/Signuppage.jsx
+++ src/pages/auth/signup/Signuppage.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [userType, setUserType] = useState('');
-  const [companyName, setCompanyName] = useState('');
-  const [usn, setUsn] = useState('');
-  const [sfname, setSfname] = useState('');
-  const [slname, setSlname] = useState('');
-  const [passout, setPassout] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userType, setUserType] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [usn, setUsn] = useState("");
+  const [sfname, setSfname] = useState("");
+  const [slname, setSlname] = useState("");
+  const [passout, setPassout] = useState("");
+  const [error, setError] = useState("");
 
   const handleSignup = async (e) => {
     e.preventDefault();
@@ -26,87 +26,106 @@ const Signup = () => {
       passoutYear: passout,
     };
 
-    if (userType === 'ALUMNI') {
+    if (userType === "ALUMNI") {
       signupData.companyName = companyName;
-    } else if (userType === 'STUDENT') {
+    } else if (userType === "STUDENT") {
       signupData.usn = usn;
     }
 
     try {
       let response;
-      if (userType === 'ALUMNI') {
-        response = await axios.post("http://localhost:3004/auth/registerAlumni", signupData);
-      } else if (userType === 'STUDENT') {
-        response = await axios.post("http://localhost:3004/auth/registerStudent", signupData);
+      if (userType === "ALUMNI") {
+        response = await axios.post(
+          "http://localhost:3004/auth/registerAlumni",
+          signupData
+        );
+      } else if (userType === "STUDENT") {
+        response = await axios.post(
+          "http://localhost:3004/auth/registerStudent",
+          signupData
+        );
       }
 
       if (response.status === 201) {
-        alert('Signup successful! Please login.');
-        navigate('/login');
+        alert("Signup successful! Please login.");
+        navigate("/login");
       } else {
-        throw new Error('Signup failed');
+        throw new Error("Signup failed");
       }
     } catch (err) {
-      setError('Signup failed. Please try again.');
+      setError("Signup failed. Please try again.");
       console.error(err);
     }
   };
 
   return (
-    <div className="w-full flex bg-white">
-      <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
-        <div className="text-sm text-blue-200">
-          <a className="hover:text-blue-800" href="https://central.sjceplacements.org/">
-            Click here to redirect to the college placement website
-          </a>
-        </div>
+    <div className="flex flex-col lg:flex-row bg-white">
+      <div className="w-full lg:w-1/2 flex justify-center items-center">
+        <div className="max-w-md py-8 px-4 w-full">
+          <div className="text-sm text-blue-200 mb-4">
+            <a
+              className="hover:text-blue-800"
+              href="https://central.sjceplacements.org/"
+            >
+              Click here to redirect to the college placement website
+            </a>
+          </div>
 
-        <div className="flex justify-center pt-32 md:pt-4">
-          <a href="#">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Student_%28example%29.svg/888px-Student_%28example%29.svg.png"
-              className="w-20 h-20"
-              alt="Logo"
-            />
-          </a>
-        </div>
-        <div className="flex flex-col justify-center md:justify-start my-2 pt-8 md:pt-1 px-8 md:px-24 lg:px-32">
-          <h2 className="text-center text-black">Signup</h2>
-          {error && <p className="text-red-500">{error}</p>}
-          <div className="py-2 px-1">
+          <div className="flex justify-center md:pt-4">
+            <a href="#">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Student_%28example%29.svg/888px-Student_%28example%29.svg.png"
+                className="w-20 h-20"
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <h2 className="text-center text-3xl">Signup</h2>
+          {error && <p className="text-red-500 mb-4">{error}</p>}
+          <div className="flex justify-between mb-4">
             <button
-              className={`bg-black text-white font-semibold py-2 px-10 mx-1 border border-gray-500 rounded ${userType === 'ALUMNI' ? 'selected' : ''}`}
-              onClick={() => setUserType('ALUMNI')}
+              className={`py-2 px-6 text-white font-semibold border border-gray-500 rounded ${
+                userType === "ALUMNI" ? "bg-gray-800" : "bg-gray-300"
+              }`}
+              onClick={() => setUserType("ALUMNI")}
             >
               ALUMNI
             </button>
             <button
-              className={`bg-black text-white font-semibold py-2 px-10 mx-1 border border-gray-500 rounded ${userType === 'ADMIN' ? 'selected' : ''}`}
-              onClick={() => setUserType('ADMIN')}
+              className={`py-2 px-6 text-white font-semibold border border-gray-500 rounded ${
+                userType === "ADMIN" ? "bg-gray-800" : "bg-gray-300"
+              }`}
+              onClick={() => setUserType("ADMIN")}
             >
               ADMIN
             </button>
             <button
-              className={`bg-black text-white font-semibold py-2 px-10 mx-1 border border-gray-500 rounded ${userType === 'STUDENT' ? 'selected' : ''}`}
-              onClick={() => setUserType('STUDENT')}
+              className={`py-2 px-6 text-white font-semibold border border-gray-500 rounded ${
+                userType === "STUDENT" ? "bg-gray-800" : "bg-gray-300"
+              }`}
+              onClick={() => setUserType("STUDENT")}
             >
               STUDENT
             </button>
           </div>
-          <form className="flex flex-col pt-3 md:pt-8" onSubmit={handleSignup}>
-            <div className="flex flex-col pt-4">
-              <label htmlFor="username" className="text-lg">Username</label>
+          <form onSubmit={handleSignup} className="space-y-4">
+            <div className="flex flex-col">
+              <label htmlFor="username" className="text-lg">
+                Username
+              </label>
               <input
                 type="text"
                 id="username"
-                placeholder="username"
+                placeholder="Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
               />
             </div>
-            <div className="flex flex-col pt-4">
-              <label htmlFor="sfname" className="text-lg">First Name</label>
+            <div className="flex flex-col">
+              <label htmlFor="sfname" className="text-lg">
+                First Name
+              </label>
               <input
                 type="text"
                 id="sfname"
@@ -116,8 +135,10 @@ const Signup = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
               />
             </div>
-            <div className="flex flex-col pt-4">
-              <label htmlFor="slname" className="text-lg">Last Name</label>
+            <div className="flex flex-col">
+              <label htmlFor="slname" className="text-lg">
+                Last Name
+              </label>
               <input
                 type="text"
                 id="slname"
@@ -127,8 +148,10 @@ const Signup = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
               />
             </div>
-            <div className="flex flex-col pt-4">
-              <label htmlFor="email" className="text-lg">Email</label>
+            <div className="flex flex-col">
+              <label htmlFor="email" className="text-lg">
+                Email
+              </label>
               <input
                 type="email"
                 id="email"
@@ -138,8 +161,10 @@ const Signup = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
               />
             </div>
-            <div className="flex flex-col pt-4">
-              <label htmlFor="password" className="text-lg">Password</label>
+            <div className="flex flex-col">
+              <label htmlFor="password" className="text-lg">
+                Password
+              </label>
               <input
                 type="password"
                 id="password"
@@ -149,9 +174,12 @@ const Signup = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
               />
             </div>
-            {userType === 'ALUMNI' && (
-              <div className="flex flex-col pt-4">
-                <label htmlFor="companyName" className="text-lg">Company Name</label>
+            {/* Additional fields based on userType */}
+            {userType === "ALUMNI" && (
+              <div className="flex flex-col">
+                <label htmlFor="companyName" className="text-lg">
+                  Company Name
+                </label>
                 <input
                   type="text"
                   id="companyName"
@@ -162,10 +190,12 @@ const Signup = () => {
                 />
               </div>
             )}
-            {userType === 'STUDENT' && (
+            {userType === "STUDENT" && (
               <>
-                <div className="flex flex-col pt-4">
-                  <label htmlFor="usn" className="text-lg">USN</label>
+                <div className="flex flex-col">
+                  <label htmlFor="usn" className="text-lg">
+                    USN
+                  </label>
                   <input
                     type="text"
                     id="usn"
@@ -175,8 +205,10 @@ const Signup = () => {
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
                   />
                 </div>
-                <div className="flex flex-col pt-4">
-                  <label htmlFor="passout" className="text-lg">Passout Year</label>
+                <div className="flex flex-col">
+                  <label htmlFor="passout" className="text-lg">
+                    Passout Year
+                  </label>
                   <input
                     type="text"
                     id="passout"
@@ -188,18 +220,31 @@ const Signup = () => {
                 </div>
               </>
             )}
-            <input type="submit" value="Sign Up" className="bg-blue-900 text-white font-bold text-lg hover:bg-blue-200 hover:text-gray-900 py-2 px-4 rounded-xl mt-8" />
+            <center><input
+              type="submit"
+              value="Sign Up"
+              className="bg-blue-900 text-white font-bold text-lg hover:bg-blue-200 hover:text-gray-900 py-2 px-2 rounded-xl mt-8"
+            />
+            </center>
           </form>
-          <div className="text-center pt-12 pb-12">
-            <p>Already have an account? <a href="/login" className="underline font-semibold hover:bg-blue-200">Log in here</a></p>
+
+          <div className="text-center pt-8">
+            <p className="text-blue-500">
+              Already have an account?{" "}
+              <a href="/login" className="text-blue-500">
+                Log in here
+              </a>
+            </p>
           </div>
         </div>
       </div>
-      <div className="w-1/2 shadow-2xl">
-            <img className="object-cover w-screen h-full hidden md:block"
-                src="https://images.unsplash.com/photo-1567132765096-e5486ab30ecf?q=80&w=2856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Background" />
-        </div>
+      <div className="w-full lg:w-1/2 hidden lg:block">
+        <img
+          src="https://images.unsplash.com/photo-1567132765096-e5486ab30ecf?q=80&w=2856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Background"
+          className="object-cover w-full h-full"
+        />
+      </div>
     </div>
   );
 };
