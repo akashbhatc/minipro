e273af61820cbe60d8010d4248816aa27f984d39
akashbhatc <117592488+akashbhatc@users.noreply.github.com>
2024-06-01 21:19:36 +0530

    auth route updated

--- src/pages/auth/login/Loginpage.jsx
+++ src/pages/auth/login/Loginpage.jsx
@@ -75,11 +75,11 @@ const Login = () => {
           </div>
           <form className="flex flex-col pt-3 md:pt-8" onSubmit={handleLogin}>
             <div className="flex flex-col pt-4">
-              <label htmlFor="email" className="text-lg">Username</label>
+              <label htmlFor="email" className="text-lg">Email</label>
               <input
                 type="text"
-                id="username"
-                placeholder="username"
+                id="email"
+                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
